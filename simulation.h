c061c3d734ef6f8bf7abbde52f97cdf18605e792
Yusheng Cai <cys9741@seas.upenn.edu>
2021-03-05 22:38:15 -0500

    Updated file structure

--- simulation.h
+++
@@ -1,6 +0,0 @@
-#include <vector>
-double rotatespin(const double &spin);
-double randomangle();
-double E_initial(std::vector<std::vector<double>> &lattice,double J);
-void run(int N,int nsweeps,double J);
-
