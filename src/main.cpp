0359d0b6216eaee8aaec58034fbec8249c18d76f
Yusheng Cai <cys9741@seas.upenn.edu>
2021-03-14 21:58:21 -0400

    Reduced variables from J and T to KbT/J

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,22 @@
+#include "xy_model.h"
 #include <iostream>
 #include <ctime>
 #include <stdlib.h>
-#include "xy_model.h"
 #include <string>
 
 int main(int argc, char** argv){
 	std::srand(std::time(0));
 	int N = atoi(argv[1]);
 	int nsweeps = atoi(argv[2]);
-	double T = atof(argv[3]);
-	double J = atof(argv[4]);
+	double kbTJ = atof(argv[3]);
 
-	if(argc > 5){	
-		std::string energyname = argv[5];
-		std::string configname = argv[6];
-		xy_model::run(N,nsweeps,J,T,energyname,configname);
+	if(argc > 4){	
+		std::string energyname = argv[4];
+		std::string configname = argv[5];
+		xy_model::run(N,nsweeps,kbTJ,energyname,configname);
 	}
 	else{
-		xy_model::run(N,nsweeps,J,T);
+		xy_model::run(N,nsweeps,kbTJ);
 	}
 
 
