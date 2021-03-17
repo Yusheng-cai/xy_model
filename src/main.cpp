b8c7a2802d123f27b2501f02a7f5a66c6c124ab9
Yusheng Cai <cys9741@seas.upenn.edu>
2021-03-17 00:46:23 -0400

    Used getopt to parse command line argument

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,49 @@
 #include <iostream>
 #include <ctime>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string>
+#include <unistd.h>
 
-int main(int argc, char** argv){
+int main(int argc, char* argv[]){
 	std::srand(std::time(0));
-	int N = atoi(argv[1]);
-	int nsweeps = atoi(argv[2]);
-	double kbTJ = atof(argv[3]);
 
-	if(argc > 4){	
-		std::string energyname = argv[4];
-		std::string configname = argv[5];
-		xy_model::run(N,nsweeps,kbTJ,energyname,configname);
+	// command line arguments must be larger than 3 for N, nsweeps and kbTJ
+	if(argc < 4){
+		std::cerr << "You must provide: N(number of grids), nsweeps(number of sweeps to be performed), kbTJ(kbT/J)" << std::endl;
+	       	return 1;	
 	}
-	else{
-		xy_model::run(N,nsweeps,kbTJ);
-	}
+	
+	int opt;
+	int N=0;
+	int nsweeps=0;
+	int printevery=1000;
+	double kbTJ=0.0;
+	std::string energyname="energy.dat";
+	std::string propertyname="property.dat";
+	std::string configname="config.dat";
 
+	
+	for(;;){	
+		switch(getopt(argc,argv,"n:s:k:")){
+		case 'n':
+			N=atoi(optarg);
+			continue;
+		case 's':
+			nsweeps=atoi(optarg);
+			continue;
+		case 'k':
+			kbTJ=atof(optarg);
+			break;
+			}
+		break;
+	}
+	
+	std::cout << N << std::endl;
+	std::cout << nsweeps << std::endl;
+	std::cout << kbTJ << std::endl;
+	xy_model::run(N,nsweeps,kbTJ,energyname,configname,propertyname,printevery);
+	
 
 	return 0;
 }
